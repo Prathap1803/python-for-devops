6a782009bf747964bd3bf5b045ec409a39d96edc
Veeraprathap Edulapuram <135488849+Prathap1803@users.noreply.github.com>
2024-02-09 11:58:21 +0000

    updated final file

--- JIRA/create-issue.py
+++ JIRA/create-issue.py
@@ -4,7 +4,7 @@ import json
 
 url = "https://dummydotx.atlassian.net/rest/api/3/issue"
 
-API_TOKEN = "ATATT3xFfGF0DnVsEpZmlqx5S2zgOJsax1wcX1qyOGwoFt5GsrYIiAtSC6EJHWjjbfMyAOM6CMfh6_a5LgW3wf4uPlEDeY_Iel9Tojxyv5hznJV2COjo0HOOht3b_KoKIQG7N8usXSXiYIdHreIJa-9AlUcJmKfrjGvIWnLx3nFJdAneUK5MF4Y=5981BF7E"
+API_TOKEN = "ATATT3xFfGF0omDUAZ8q09K93XW2mOFDjtJfstOF0IBg9n_jvTV1pRtPFFgjLsCifiCVDuypPr8lA_ph_KPuAkWFfmk2Q8ux-BpIAfbPQTKETAXjcw0A4viwb4iKTnwyStC8D2dN-Sia-4x046DgHe8Ia2urPLLTMfvNeHUgDjUmR6xa0GLbrQc=A1205DC5"
 auth = HTTPBasicAuth("dummy.dotx@gmail.com", API_TOKEN)
 
 headers = {
--- JIRA/flask-create-issue-final.py
+++ JIRA/flask-create-issue-final.py
@@ -1,7 +1,3 @@
-
-from flask import Flask
-import requests
-import request
 from requests.auth import HTTPBasicAuth
 import json
 app = Flask(__name__)
@@ -9,17 +5,13 @@ app = Flask(__name__)
 @app.route("/createJira",methods=['POST'])
 def createJira():
 
-
     url = "https://dummydotx.atlassian.net/rest/api/3/issue"
-
     API_TOKEN = "ATATT3xFfGF0omDUAZ8q09K93XW2mOFDjtJfstOF0IBg9n_jvTV1pRtPFFgjLsCifiCVDuypPr8lA_ph_KPuAkWFfmk2Q8ux-BpIAfbPQTKETAXjcw0A4viwb4iKTnwyStC8D2dN-Sia-4x046DgHe8Ia2urPLLTMfvNeHUgDjUmR6xa0GLbrQc=A1205DC5"
     auth = HTTPBasicAuth("dummy.dotx@gmail.com", API_TOKEN)
-
     headers = {
     "Accept": "application/json",
     "Content-Type": "application/json"
     }
-
     payload = json.dumps( {
     "fields": {
 
@@ -39,33 +31,29 @@ def createJira():
         "type": "doc",
         "version": 1
         },
-
-
-
         "issuetype": {
         "id": "10019"
         },
-
         "project": {
         "key": "MD"
         },
-
         "summary": "Main order flow broken",
-
-
     },
     "update": {}
     } )
+    data1 = json.loads(request.data)
 
-    response = requests.request(
-    "POST",
-    url,
-    data=payload,
-    headers=headers,
-    auth=auth
-    )
-    data = json.loads(request.data)
-    return data 
-    return(((response.text) ))
-    
+    if (data1["comment"]["body"])== "/jira" :
+        response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+        )
+        #print (request.headers['X-GitHub-Event'])
+        #print (type(data1))
+        return((response.text))
+     else:
+        print ("Please enter valid comments  : ")
 app.run('0.0.0.0')
